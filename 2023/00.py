6319f806b6aa95f6e810531934a63928efe40024
Will Cravitz <wcravitz@uchicago.edu>
2023-12-24 16:36:32 -0600

    added a template file

---
+++ 2023/00.py
@@ -0,0 +1,19 @@
+# ===== PART 1 ===== #
+
+def part_one() -> None:
+    pass
+    
+
+# ===== PART 2 ===== #
+
+def part_two() -> None:
+    pass
+
+
+# ===== SOLUTIONS ===== # 
+
+if __name__ == "__main__":
+    print("===== Part 1 =====")
+    part_one()
+    print("===== Part 2 =====")
+    part_two()
